46f64e521e57dd96577f41390a213bf03d2ce7a6
Ashwin <ashwin.thanga@gmail.com>
2024-04-16 00:12:08 +0530

    make the todo adder as a component of the todolist

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,21 +1,17 @@
 import "./App.css";
 import Header from "./Components/Header.js";
-import Todoadd from "./Components/TodoCard_Adder.js";
+
 import TodoCardList from "./Components/TodoCardList.js";
-import { useState } from "react";
 
 function App() {
-  const [data, setdata] = useState([]);
-  const handledata = (senddata) => {
-    setdata([...data, senddata]);
-  };
-  console.log(data);
-
   return (
-    <div>
+    <div className="app-container">
       <Header />
-      <Todoadd sendata={handledata} />
-      <TodoCardList data={data} />
+      <div className="content-container">
+        <div className="right-half">
+          <TodoCardList />
+        </div>
+      </div>
     </div>
   );
 }
--- todo_app/src/Components/TodoCardList.js
+++ todo_app/src/Components/TodoCardList.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { ImBin } from "react-icons/im";
+import "./TodoCardList.css";
+import Todoadd from "./TodoCard_Adder";
 
-const TodoCardList = (props) => {
-  const todo = props.data;
+const TodoCardList = () => {
+  const [data, setData] = useState([]);
+
+  const handleData = (senddata) => {
+    setData([...data, senddata]);
+  };
+  const handle_Delete = (id) => {
+    const newData = data.filter((task) => task.id !== id);
+    setData(newData);
+  };
 
   return (
-    <div>
+    <div className="todo_list">
       <h2>WORK LIST</h2>
-      {todo.map((todos) => (
+
+      <Todoadd sendata={handleData} />
+      {data.map((todos) => (
         <div className="work_preview" key={todos.id}>
-          <h4>{todos.id}</h4>
           <p>Work Name is {todos.title}</p>
           <p>description is {todos.description}</p>
+          <ImBin onClick={() => handle_Delete(todos.id)} />
         </div>
       ))}
     </div>
